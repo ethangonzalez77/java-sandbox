a7e0dd134c06df7f3ae1c21767d0e13070a63cee
ethan <ethan.gonzalez00774@gmail.com>
2023-01-16 15:00:35 -0800

    scanner app displays lowest/highest number

--- src/Ex.java
+++ src/Ex.java
@@ -1,4 +1,4 @@
-import org.w3c.dom.ls.LSOutput;
+
 
 import java.util.Scanner;
 
@@ -55,10 +55,10 @@ public class Ex {
             int order = counter + 1;
 
             System.out.println("Prompt: " + order + " Enter any number:");
-            boolean isInt = sc1.hasNextInt();
+            boolean hasInt = sc1.hasNextInt();
 
 
-            if (isInt) {
+            if (hasInt) {
                 sum +=  sc1.nextInt();
                 counter++;
 
@@ -81,6 +81,57 @@ public class Ex {
 
 
 
+        //Scanner application that prompts the user to enter as many numbers as they want, and prints out there lowest number and their highest number.
+
+
+
+        Scanner sc3 = new Scanner(System.in);
+
+        boolean flag = true;
+        int min = 0;
+        int max = 0;
+
+
+
+        while(true) {
+
+            System.out.println("Enter a number: ");
+            boolean hasInt = sc3.hasNextInt();
+
+
+
+            if (hasInt) {
+
+                int userInput = sc3.nextInt();
+
+                if (flag) {
+                    flag = false;
+                    min = userInput;
+                    max = userInput;
+                }
+
+                if (userInput < min) {
+                    min = userInput;
+                }
+
+
+                if (userInput > max) {
+                    max = userInput;
+                }
+
+            }else{
+                System.out.println("Exiting program...");
+                break;//always use a break, if not you'll get an error for the close() saying its a un reachable statement.
+            }
+
+            sc3.nextLine();
+
+        }
+
+        sc3.close();
+        System.out.println();
+        System.out.println("the min: " + min);
+        System.out.println("the max: " + max);
 
 
 
@@ -89,12 +140,63 @@ public class Ex {
 
 
 
+
+        Scanner sc4 = new Scanner(System.in);
+
+        boolean flag2 = true;
+        int min2 = 0;
+        int max2 = 0;
+
+    while (true) {
+
+        System.out.println("Enter a number: ");
+        System.out.println("To exit the program press any letter on the key board.");
+        boolean hasInt = sc4.hasNextInt();
+
+        if (hasInt) {
+
+            int usersNumber = sc4.nextInt();//first check if the users input is a ligit number, then parse the String into an int.
+
+            if(flag2) {
+                flag2 = false;
+                min2 = usersNumber;
+                max2 = usersNumber;
+            }
+            if(usersNumber < min2) {
+                min2 = usersNumber;
+            }
+            if(usersNumber > max2) {
+                max2 = usersNumber;
+            }
+
+        }else {
+            System.out.println("Exiting program...");
+            break;
+        }
+        sc4.nextLine();
     }
 
+    sc4.close();//remember to add a break statement to reach this part of the code.
+        System.out.println("Your min number is : " + min2);
+        System.out.println("Your max number is : " + max2);
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+        //end of the main method.
+
+    }
 
 }
 
