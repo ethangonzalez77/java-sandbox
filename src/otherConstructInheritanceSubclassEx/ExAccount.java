f671c55dd89faadb0a3b3d8fd47262bc05fe3b12
ethan <ethan.gonzalez00774@gmail.com>
2023-01-21 18:19:52 -0800

    moved some stuff around, nothing major

--- src/otherConstructInheritanceSubclassEx/ExAccount.java
+++ src/otherConstructInheritanceSubclassEx/ExAccount.java
@@ -3,21 +3,21 @@ package otherConstructInheritanceSubclassEx;
 public class ExAccount {
     public static void main(String[] args) {
 
-    Account et = new Account();
+        Account et = new Account();
 
-    et.setNumber("24");
-    et.setBalance(100);
-    et.setCustomerName("elee");
-    et.setCustomerPhoneNumber("000-111-222-3333");
-    et.setCustomerEmailAddress("el00774@gmail.com");
+        et.setNumber("24");
+        et.setBalance(100);
+        et.setCustomerName("elee");
+        et.setCustomerPhoneNumber("000-111-222-3333");
+        et.setCustomerEmailAddress("el00774@gmail.com");
 
-    System.out.println(et.getNumber());
-    System.out.println(et.getBalance());
-    System.out.println(et.getCustomerName());
-    System.out.println(et.getCustomerPhoneNumber());
-    System.out.println(et.getCustomerEmailAddress());
+        System.out.println(et.getNumber());
+        System.out.println(et.getBalance());
+        System.out.println(et.getCustomerName());
+        System.out.println(et.getCustomerPhoneNumber());
+        System.out.println(et.getCustomerEmailAddress());
 
-    //now with the constructor
+        //now with the constructor
 
         System.out.println("-------------------------------------");
 
@@ -28,6 +28,10 @@ public class ExAccount {
         System.out.println(a1.getCustomerName());
         System.out.println(a1.getCustomerEmailAddress());
         System.out.println(a1.getCustomerPhoneNumber());
+
+        //now with a empty constructor that has default values that are applied to the Class's fields
+
+
 
 
 
