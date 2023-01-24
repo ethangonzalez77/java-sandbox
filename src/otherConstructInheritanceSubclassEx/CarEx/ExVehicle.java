753c9b9adb664a3ec7945cbd3551f982f6a6e909
ethan <ethan.gonzalez00774@gmail.com>
2023-01-23 21:34:10 -0800

    object with  methods to print out

--- src/otherConstructInheritanceSubclassEx/CarEx/ExVehicle.java
+++ src/otherConstructInheritanceSubclassEx/CarEx/ExVehicle.java
@@ -22,7 +22,23 @@ public class ExVehicle {
 
 
 
-        iz1.accelerate(50);
+//        iz1.accelerate(50);
+
+        System.out.println(iz1.getCurrentVelocity());
+        System.out.println(iz1.getCurrentDirection());
+
+
+        System.out.println("----------------------------------");
+
+        iz1.accelerate(0);
+        iz1.accelerate(10);
+        iz1.accelerate(20);
+        iz1.accelerate(30);
+        iz1.accelerate(40);
+        iz1.accelerate(900);
+
+        Impereza iz2 = new Impereza(9000);
+
 
 
 
